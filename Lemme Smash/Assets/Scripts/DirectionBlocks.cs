cc5419604c26d46a8e0ae0ea95c2ae60bf314022
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2019-07-27 11:26:12 -0400

    DirectionBlocks script basic input

---
+++ Lemme Smash/Assets/Scripts/DirectionBlocks.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionBlocks : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode left;
+
+    [SerializeField]
+    private KeyCode right;
+
+    [SerializeField]
+    private KeyCode up;
+
+    [SerializeField]
+    private KeyCode down;
+
+    [SerializeField]
+    private KeyCode becky;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(left))
+        {
+            //Debug.Log("LEFT!");
+        }
+        else if (Input.GetKeyDown(right))
+        {
+            //Debug.Log("RIGHT!");
+        }
+        else if (Input.GetKeyDown(up))
+        {
+            //Debug.Log("UP!");
+        }
+        else if (Input.GetKeyDown(down))
+        {
+            //Debug.Log("DOWN!");
+        }
+        else if (Input.GetKeyDown(becky))
+        {
+            //Debug.Log("BECKY!");
+        }
+    }
+
+    private enum ButtonType
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        BECKY,
+        COMBO_BREAKER
+    }
+}
