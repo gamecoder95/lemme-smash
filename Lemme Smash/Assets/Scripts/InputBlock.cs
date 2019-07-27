a7de0aa172231ad3c711d97d471bc6d3087605b2
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2019-07-27 15:50:11 -0400

    Simple integration of scripts

--- Lemme Smash/Assets/Scripts/InputBlock.cs
+++ Lemme Smash/Assets/Scripts/InputBlock.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DirectionBlock : MonoBehaviour
+public class InputBlock : MonoBehaviour
 {
-
     [SerializeField]
     private KeyCode keyCode;
 
     private bool valid;
+    private SpriteRenderer spriteRenderer;
+
+    private const string COMBO_BREAKER_TAG = "ComboBreaker";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,21 +26,36 @@ public class DirectionBlock : MonoBehaviour
             if (valid)
             {
                 Debug.Log("HIT!");
+                spriteRenderer.color = new Color(255f, 255f, 0f, 255f);
             }
             else
             {
                 Debug.Log("MISS!");
+                spriteRenderer.color = new Color(255f, 0f, 175f, 255f);
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        valid = true;
+        SetValidTrue(other.gameObject.tag);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        SetValidTrue(other.gameObject.tag);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         valid = false;
+    }
+
+    private void SetValidTrue(string otherTag)
+    {
+        if (otherTag != COMBO_BREAKER_TAG)
+        {
+            valid = true;
+        }
     }
 }
