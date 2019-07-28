e9616e5d4eec456b4de2614a952d9b285676b65e
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2019-07-28 17:41:55 -0400

    Set up the animations

--- Lemme Smash/Assets/Scripts/InputBlock.cs
+++ Lemme Smash/Assets/Scripts/InputBlock.cs
@@ -23,6 +23,8 @@ public class InputBlock : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Color originalColor;
 
+    public Animator animator;
+
     public delegate void InputPressedFunc();
     public InputPressedFunc ValidHitCallback
     {
@@ -50,6 +52,8 @@ public class InputBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetInteger("isAHit", 0);
+        animator.SetInteger("isInputting", 0);
         // Check each input press: reward for the successful press and punish for each unsuccessful press
         foreach (KeyValuePair<KeyCode, string> entry in inputToArrowMapping)
         {
@@ -57,10 +61,14 @@ public class InputBlock : MonoBehaviour
 
             if (Input.GetKeyDown(entry.Key))
             {
+                animator.SetInteger("isInputting", 1);
+
                 if (arrowTag == entry.Value)
                 {
                     //Debug.Log("HIT!");
                     ValidHitCallback();
+
+                    animator.SetInteger("isAHit", 1); //Tells the animator is a hit and not a miss for the animation
 
                     // Replace this with better animation
                     spriteRenderer.color = new Color(255f, 255f, 0f, 255f);
@@ -72,6 +80,7 @@ public class InputBlock : MonoBehaviour
                 {
                     //Debug.Log("MISS!");
                     MissCallback();
+
                 }
             }
 
---
+++ Lemme Smash/Assets/Scripts/P1_Anim.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1_Anim : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode leftKeyCode;
+
+    [SerializeField]
+    private KeyCode downKeyCode;
+
+    [SerializeField]
+    private KeyCode upKeyCode;
+
+    [SerializeField]
+    private KeyCode rightKeyCode;
+
+    public Animator animator;
+
+    void Update()
+    {
+        if (Input.GetKey(leftKeyCode))
+        {
+            animator.SetInteger("Left", 1);
+        }
+
+        else
+        {
+            animator.SetInteger("Left", 0);
+        }
+
+        if (Input.GetKey(downKeyCode))
+        {
+            animator.SetInteger("Down", 1);
+        }
+
+        else
+        {
+            animator.SetInteger("Down", 0);
+        }
+
+        if (Input.GetKey (upKeyCode))
+        {
+            animator.SetInteger("Up", 1);
+        }
+
+        else
+        {
+            animator.SetInteger("Up", 0);
+        }
+
+        
+
+        if (Input.GetKey(rightKeyCode))
+        {
+            animator.SetInteger("Right", 1);
+        }
+
+        else
+        {
+            animator.SetInteger("Right", 0);
+        }
+
+        
+    }
+}
